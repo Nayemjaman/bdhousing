142774e4088e7134528822f924e8af9aa5657a1d
Nayem Jaman Tusher <njaman26@gmail.com>
2022-04-27 18:18:53 +0600

    Owner Details

--- bdhousing_scraper/settings.py
+++ bdhousing_scraper/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = 'bdhousing_scraper.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'bdhousing_scraper (+http://www.yourdomain.com)'
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- bdhousing_scraper/spiders/bdhousing.py
+++ bdhousing_scraper/spiders/bdhousing.py
@@ -12,4 +12,21 @@ class BdhousingSpider(scrapy.Spider):
         housing_links = ['https://www.bdhousing.com'+i for i in housing_links]
         yield from response.follow_all(housing_links[:2], self.parse_details)
 
-    
+    def parse_details(self, response):
+        title = (unicodedata.normalize("NFKD",response.css('h1 ::text').extract_first())).strip()
+        location = (unicodedata.normalize("NFKD",response.css('h1 +p ::text').extract_first())).strip()
+        price = (unicodedata.normalize("NFKD",response.css('.ct-productID ::text').extract_first())).strip()
+        # PROPERTY SUMMARY
+        heading = response.css('.detail-list label ::text').getall()
+        heading = [i.strip() for i in heading]
+        summary = response.css('.detail-list span ::text').getall()
+        summary = [i.strip() for i in summary]
+        
+        #PROPERTY FEATURES
+        features = response.css('.col-xs-6 label ::text').getall()
+        features = [i.strip() for i in features]
+        # Property Owner Details
+        companey_name = response.css('h5 a ::text').get()
+        property_id = response.css('h5 +p ::text').get()
+        property_id = property_id.split(':')[1].strip()
+        print(price)
